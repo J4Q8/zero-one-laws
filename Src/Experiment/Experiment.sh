5856c9861b1d50b37c533c24f2876ea7dd875ff3
J.Z.Lucki@student.rug.nl <s3986209@peregrine.hpc.rug.nl>
2022-06-20 21:40:26 +0200

    bash cmd modified

--- Src/Experiment/Experiment.sh
+++ Src/Experiment/Experiment.sh
@@ -3,8 +3,8 @@
 #SBATCH --time=10-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4gb
-#SBATCH --array=1-36
+#SBATCH --mem-per-cpu=2gb
+#SBATCH --array=1-180
 
 
 module purge
